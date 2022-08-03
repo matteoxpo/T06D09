932457e11dd502408cdb09820e6ad510504993a1
Kwaltz Buck <kwaltzbu@se-b5.nsk.21-school.ru>
2022-08-03 20:40:06 +0700

    style etc + quest5

--- src/key9part1.c
+++ src/key9part1.c
@@ -1,90 +1,86 @@
 /*------------------------------------
-	Здравствуй, человек!
-	Чтобы получить ключ 
-	поработай с комментариями.
+        Здравствуй, человек!
+        Чтобы получить ключ
+        поработай с комментариями.
 -------------------------------------*/
 
 #include <stdio.h>
 
 #define ArrSize 10
-int input (int *buffer, int *length);
-void output (int *buffer, int length);
+int input(int *buffer, int *length);
+void output(int *buffer, int length);
 int sum_numbers(int *buffer, int length);
-int find_numbers(int* buffer, int length, int number, int* numbers);
+int find_numbers(int *buffer, int length, int number, int *numbers);
 
 /*------------------------------------
-	Функция получает массив данных 
-	через stdin.
-	Выдает в stdout особую сумму
-	и сформированный массив 
-	специальных элементов
-	(выбранных с помощью найденной суммы):
-	это и будет частью ключа
+        Функция получает массив данных
+        через stdin.
+        Выдает в stdout особую сумму
+        и сформированный массив
+        специальных элементов
+        (выбранных с помощью найденной суммы):
+        это и будет частью ключа
 -------------------------------------*/
 int main() {
-	int mas[ArrSize];
-	int numbers[ArrSize];
-	int length;
-	if (input(mas, &length)) {
-		int sum = sum_numbers(mas, length);
-		int num_length = find_numbers(mas, length, sum, numbers);
-		printf("%d\n",sum);
-		output(numbers, num_length);
-	}
+  int mas[ArrSize];
+  int numbers[ArrSize];
+  int length;
+  if (input(mas, &length)) {
+    int sum = sum_numbers(mas, length);
+    int num_length = find_numbers(mas, length, sum, numbers);
+    printf("%d\n", sum);
+    output(numbers, num_length);
+  }
 }
 
 int input(int *buffer, int *length) {
-    char c = 0;
-	int check = 1;
-    if (scanf("%d%c", length, &c) && c == '\n' && *length >= 0 && *length <= ArrSize) {
-        for (int *p = buffer; p - buffer < *length; p++)
-            if (scanf("%d%c", p, &c) && (c == ' ' || c == '\n' || c == EOF)) {
-                continue;
-            } else {
-                printf("n/a");
-				check = 0;
-            }
-    } else {
+  char c = 0;
+  int check = 1;
+  if (scanf("%d%c", length, &c) && c == '\n' && *length >= 0 &&
+      *length <= ArrSize) {
+    for (int *p = buffer; p - buffer < *length; p++)
+      if (scanf("%d%c", p, &c) && (c == ' ' || c == '\n' || c == EOF)) {
+        continue;
+      } else {
         printf("n/a");
-		check = 0;
-    }
-	return check;
+        check = 0;
+      }
+  } else {
+    printf("n/a");
+    check = 0;
+  }
+  return check;
 }
 
 void output(int *buffer, int length) {
-    for (int *p = buffer; p - buffer < length - 1; p++)
-        printf("%d ", *p);
-    printf("%d", *(buffer + length - 1));
+  for (int *p = buffer; p - buffer < length - 1; p++) printf("%d ", *p);
+  printf("%d", *(buffer + length - 1));
 }
 
-
-
-
 /*------------------------------------
-	Функция должна находить
-	сумму четных элементов 
-	с 0-й позиции.
+        Функция должна находить
+        сумму четных элементов
+        с 0-й позиции.
 -------------------------------------*/
 int sum_numbers(int *buffer, int length) {
-	int sum = 0;
-	for (int i = 1; i < length; i++)
-		if ( *(buffer + i) % 2 == 0)
-			sum = sum + *(buffer + i);
-	return sum;
+  int sum = 0;
+  for (int i = 1; i < length; i++)
+    if (*(buffer + i) % 2 == 0) sum = sum + *(buffer + i);
+  return sum;
 }
 
 /*------------------------------------
-	Функция должна находить
-	все элементы, на которые нацело
-	делится переданное число и
-	записывает их в выходной массив.
+        Функция должна находить
+        все элементы, на которые нацело
+        делится переданное число и
+        записывает их в выходной массив.
 -------------------------------------*/
-int find_numbers(int* buffer, int length, int number, int* numbers) {
-	int num_length = 0;
-	for (int i = 0; i < length; i++)
-		if (*(buffer + i) != 0 && number % *(buffer + i) == 0) {
-			*(numbers + num_length) = *(buffer + i);
-			num_length++;
-		}
-	return num_length;
+int find_numbers(int *buffer, int length, int number, int *numbers) {
+  int num_length = 0;
+  for (int i = 0; i < length; i++)
+    if (*(buffer + i) != 0 && number % *(buffer + i) == 0) {
+      *(numbers + num_length) = *(buffer + i);
+      num_length++;
+    }
+  return num_length;
 }
--- src/key9part2.c
+++ src/key9part2.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
 #define LEN 100
-int input (int *mas, int *length);
-void output (int *mas, int length);
-void sum(int *buff1, int len1, int *buff2, int len2, int *result, int *result_length);
-void sub(int *buff1, int len1, int *buff2, int len2, int *result, int *result_length);
-void rev(int * a, int i, int j);
+int input(int *mas, int *length);
+void output(int *mas, int length);
+void sum(int *buff1, int len1, int *buff2, int len2, int *result,
+         int *result_length);
+void sub(int *buff1, int len1, int *buff2, int len2, int *result,
+         int *result_length);
+int min(int x, int y);
+void rev(int *a, int i, int j);
+int correctSymbol(char c);
+void clear_arr(int *result);
 /*
     Беззнаковая целочисленная длинная арифметика
     с использованием массивов.
-    Ввод: 
+    Ввод:
      * 2 длинных числа в виде массивов до 100 элементов
      * В один элемент массива нельзя вводить число > 9
-    Вывод: 
+    Вывод:
      * Результат сложения и разности чисел-массивов
     Пример:
      * 1 9 4 4 6 7 4 4 0 7 3 7 0 9 5 5 1 6 1
@@ -22,84 +27,110 @@ void rev(int * a, int i, int j);
        1 9 4 4 6 7 4 4 0 7 3 7 0 9 5 5 1 3 2
 */
 
-
-
-int main(){
-    int buff1[LEN];
-    int buff2[LEN];
-    int res[LEN + 1];
-    int len1;
-    int len2;
-    int lenres;
-    if (input(buff1, &len1) && input(buff2, &len2)) {
-        //printf("%d\n", len1);
-        //output(buff1, len1);
-        //sum(buff1, len1, buff2, len2, res, &lenres);
-        output(buff1,len1);
-    } else {
-      printf("n/a");
-    }
-    return 0;
+int main() {
+  int buff1[LEN];
+  int buff2[LEN];
+  int res[LEN + 1];
+  int len1;
+  int len2;
+  int lenres;
+  if (input(buff1, &len1) && input(buff2, &len2)) {
+    rev(buff1, 0, len1 - 1);
+    rev(buff2, 0, len2 - 1);
+    sum(buff1, len1, buff2, len2, res, &lenres);
+    output(res, lenres);
+    printf("\n");
+    sub(buff1, len1, buff2, len2, res, &lenres);
+    output(res, lenres);
+  } else {
+    printf("n/a");
+  }
+  return 0;
 }
-
-int input(int *mas, int* length) {
-    int check = 1;
-    int input;
-    char c;
-    *length = 0;
-    int zalupa = 1;
-    while (1) {
-      if (zalupa && scanf("%d%c", &input, &c) == 2 && c != '\n') {
-        mas[*length] = input;
-        (*length)++;
-      }
-      if (c == '\n') {
-        printf("fuck");
-        mas[*length] = input;
-        (*length)++;
-        zalupa = 0;
-        break;
-      }
-      if (c != '\n' && c != ' ') {
+int correctSymbol(char c) { return c == '\n' || c == ' ' || c == EOF ? 1 : 0; }
+int input(int *mas, int *length) {
+  int check = 1;
+  int inp;
+  char c;
+  *length = 0;
+  while (1) {
+    if (scanf("%d%c", &inp, &c) == 2 && correctSymbol(c)) {
+      if (inp / 10 != 0) {
         check = 0;
         break;
       }
-
+      mas[*length] = inp;
+      (*length)++;
+      if (c == '\n') {
+        break;
+      }
+    } else {
+      check = 0;
+      break;
     }
-    return check;
+  }
+  return check;
 }
-
 
 void output(int *mas, int length) {
-    for (int *p = mas; p - mas < length - 1; p++)
-        printf("%d ", *p);
-    printf("%d", *(mas + length - 1));
+  for (int *p = mas; p - mas < length - 1; p++) printf("%d ", *p);
+  printf("%d", *(mas + length - 1));
 }
 
-void sum(int *buff1, int len1, int *buff2, int len2, int *result, int *result_length) {
-  rev(buff1, 0, len1 - 1);
-  rev(buff2, 0, len2 - 1);
-  result = 0;
+void clear_arr(int *result) {
+  for (int i = 0; i < LEN + 2; i++) result[i] = 0;
+}
+
+void sum(int *buff1, int len1, int *buff2, int len2, int *result,
+         int *result_length) {
   *result_length = len1;
-  for (int i = 0; i < len1; i++) {
-    result[i] = buff1[i] + buff2[i];
+  clear_arr(result);
+
+  for (int i = 0; i < min(len1, len2); i++) {
+    result[i] += buff1[i] + buff2[i];
     if (result[i] / 10 != 0) {
       result[i + 1] += result[i] / 10;
       result[i] %= 10;
-      if (i == len1 - 1)
-        (*result_length)++;  
+      if (i + 1 == min(len1, len2)) (*result_length)++;
     }
   }
-  rev(buff1, 0, len1 - 1);
-  rev(buff2, 0, len2 - 1);
+  if (len2 < len1) {
+    for (int i = len2; i < len1; i++) result[i] = buff1[i];
+  }
+
+  rev(result, 0, *result_length - 1);
 }
 
-void rev(int * a, int i, int j) {
-    while(i < j) {
-        int tmp = a[i]; 
-        a[i] = a[j]; 
-        a[j] = tmp;
-        i++; 
-        j--;
+void sub(int *buff1, int len1, int *buff2, int len2, int *result,
+         int *result_length) {
+  clear_arr(result);
+
+  *result_length = len1;
+  for (int i = 0; i < min(len1, len2); i++) {
+    if (buff1[i] < buff2[i]) {
+      buff1[i] += 10;
+      buff1[i + 1] -= 1;
+      if (i + 1 == len1) {
+        (*result_length)--;
+      }
     }
+
+    result[i] = buff1[i] - buff2[i];
+  }
+  if (len2 < len1) {
+    for (int i = len2; i < len1; i++) result[i] = buff1[i];
+  }
+  rev(result, 0, *result_length - 1);
 }
+
+void rev(int *a, int i, int j) {
+  while (i < j) {
+    int tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+    i++;
+    j--;
+  }
+}
+
+int min(int x, int y) { return x > y ? y : x; }
